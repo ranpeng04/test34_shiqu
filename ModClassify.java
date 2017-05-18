40bba9e8f16b5b9e03550f3cba21bb824fcf2672
ranpeng04 <879070657@qq.com>
2017-05-18 14:45:50 +0800

    修改主分类名称

---
+++ ModClassify.java
@@ -0,0 +1,58 @@
+package com.shiqu.rp;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ModClassify {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		WebDriver wd = new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("http://139.199.228.115:86/login");
+		
+		//登录
+		wd.findElement(By.id("username")).sendKeys("admin");
+		Thread.sleep(2000);
+		wd.findElement(By.id("password")).sendKeys("admin");
+		Thread.sleep(2000);
+		wd.findElement(By.id("login")).click();
+		Thread.sleep(2000);
+		Alert alert = wd.switchTo().alert();              //弹出框
+		alert.accept();                                   //确认
+		Thread.sleep(5000);
+		
+		//食谱主分类
+		WebElement mainclassify = wd.findElement(By.xpath(".//*[@id='menu-academico']/a"));
+		Actions action2 = new Actions(wd);
+		action2.moveToElement(mainclassify).click().perform();
+		WebElement click2 = wd.findElement(By.xpath(".//*[@id='menu-academico']/ul/li[2]/a"));
+		action2.clickAndHold(click2).perform();
+		Thread.sleep(2000);
+		action2.release(click2).perform();
+		Thread.sleep(2000);
+		
+		//点击编辑按钮
+		wd.findElement(By.xpath("html/body/div[1]/div[2]/div[2]/div/div/table/tbody/tr[5]/td[3]/a[1]/i")).click();
+		//输入新的名称“营养度”
+		wd.findElement(By.name("mclassify.mclassifyName")).clear();
+		Thread.sleep(2000);
+		wd.findElement(By.name("mclassify.mclassifyName")).sendKeys("营养度");
+		//点击提交
+		wd.findElement(By.xpath("html/body/div[1]/div[2]/div[2]/div/form/fieldset/div[2]/input")).click();
+
+		try{
+			Thread.sleep(10000);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		wd.quit();
+		
+	}
+
+}
